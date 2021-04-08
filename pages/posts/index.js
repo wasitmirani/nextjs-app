83bd67c09a719eaa67fc20d40e7d56b96a13dce6
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2021-04-08 23:34:13 +0500

    done

---
+++ pages/posts/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Posts extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello world</h1>
+                
+            </div>
+        )
+    }
+}
+
+export default Posts
